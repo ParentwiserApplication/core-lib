f74561119c7fd880afaaff1cc573afa3900068ca
çağrı orhan <cagriorhan@cagrs-Air.hgw.local>
2024-08-14 13:58:25 +0300

    send push notification and send task email functions added.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const UserApiClient = require('./UserApiClient');
 const StreamApiClient = require('./StreamApiClient');
+const NotificationApiClient = require('./NotificationApiClient')
 
 module.exports = {
     UserApiClient,
-    StreamApiClient
+    StreamApiClient,
+    NotificationApiClient
 };
